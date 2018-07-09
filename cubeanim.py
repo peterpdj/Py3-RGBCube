f5d9009d9e3826639114e46362041bd63c8c1d04
Peter de Jong <ik@hier.nu>
2018-07-09 10:13:40 +0000

    Cube driver in separate process using shared memory video buffer.

--- cubeanim.py
+++ cubeanim.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 
 import time
-import threading
 import cubedriver
 
 
 class AnimationRunner:
-    """
-    This class name says it all. This is running the animation file and uses
-    cubedriver to shift the registers via SPI.
-    A double buffer is used.
-    """
-    def __init__(self, anim):
+	"""
+	This class name says it all. This is running the animation file and uses
+	cubedriver to shift the registers via SPI.
+	A double buffer is used.
+	"""
+	def __init__(self, anim, driver):
 		self.anim = anim
+		self.driver = driver
 
 	def run(self):
 		t0 = time.time()
@@ -21,10 +21,10 @@ class AnimationRunner:
 		self.anim.start()
 		while True:
 			t = time.time() - t0
-			buf = cubedriver.Buffer()
-			self.anim.draw(buf, t)
-			cubedriver.buf = buf
-			time.sleep(0.02)
+			frame = self.driver.make_frame()
+			self.anim.draw(frame, t)
+			self.driver.fill(frame)
+			time.sleep(0.01)
 			f += 1
 			if f == 1000:
 				fps = f / (t - tf0)
@@ -34,9 +34,9 @@ class AnimationRunner:
 
 
 class Animation:
-    """
-    This class is not used at this moment.
-    """
+	"""
+	This class is not used at this moment.
+	"""
 	def __init__(self):
 		pass
 
@@ -48,9 +48,9 @@ class Animation:
 
 
 class Position:
-    """
-    This class is used by the animation file.
-    """
+	"""
+	This class is used by the animation file.
+	"""
 	def __init__(self, x = 0, y = 0, z = 0):
 		self.x = x
 		self.y = y
@@ -58,9 +58,9 @@ class Position:
 
 
 class Color:
-    """
-    This class is used by the animation file.
-    """
+	"""
+	This class is used by the animation file.
+	"""
 	def __init__(self, r = 0, g = 0, b = 0):
 		self.r = r
 		self.g = g
@@ -68,19 +68,18 @@ class Color:
 
 
 def runAnimation(anim):
-    """
-    This class runs the function 'run' of class Animationrunner. Besides that
-    is uses threading for running the cubedriver in a loop.
-    """
-	driver_thread = threading.Thread(target=cubedriver.mainloop, args=())
-	driver_thread.start()
+	"""
+	This class runs the function 'run' of class Animationrunner. Besides that
+	is uses threading for running the cubedriver in a loop.
+	"""
+	driver = cubedriver.Driver()
+	driver.run()
 
 	try:
-		runner = AnimationRunner(anim)
+		runner = AnimationRunner(anim, driver)
 		runner.run()
 	except KeyboardInterrupt:
 		print('KeyboardInterrupt')
-		cubedriver.quit = True
-		driver_thread.join()
-		print('Driver thread stopped')
+		driver.stop()
+		print('Driver process stopped')
 
--- cubedriver.py
+++ cubedriver.py
@@ -1,59 +1,86 @@
 #!/usr/bin/env python3
-
+import multiprocessing
+from multiprocessing.sharedctypes import RawArray, RawValue
+import array
 import time
 import spidev
 
-quit = False
 
-anode = [0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80]
 class Buffer:
-    """
-    This buffer is used to fill one frame of the animation on the cube.
-    """
+	"""
+	This buffer is used to fill one frame of the animation on the cube.
+	"""
+	RED_OFFSET = 0
+	GREEN_OFFSET = 64
+	BLUE_OFFSET = 128
+
+	MEM_SIZE = 3 * 64
+
 	def __init__(self):
-		self.byte_red0 = bytearray(64)
-		self.byte_blue0 = bytearray(64)
-		self.byte_green0 = bytearray(64)
+		self.mem = bytearray(self.MEM_SIZE)
 
 	def LED(self, pos, color):
 		whichbyte = int(((pos.x*64)+(pos.y*8)+pos.z)/8)
 		wholebyte = (pos.x*64)+(pos.y*8)+pos.z
 		posInByte = wholebyte-(8*whichbyte)
-		#print(posInByte)
-		self.byte_red0[whichbyte] += bitRead(color.b,0)*posInByteHelp[posInByte]
-		self.byte_green0[whichbyte] += bitRead(color.g,0)*posInByteHelp[posInByte]
-		self.byte_blue0[whichbyte] += bitRead(color.r,0)*posInByteHelp[posInByte]
-		#print(whichbyte, wholebyte, byte_red0[whichbyte],byte_green0[whichbyte],byte_blue0[whichbyte])
-
-buf = Buffer()
+		self.mem[self.RED_OFFSET + whichbyte] += bitRead(color.b,0)*posInByteHelp[posInByte]
+		self.mem[self.GREEN_OFFSET + whichbyte] += bitRead(color.g,0)*posInByteHelp[posInByte]
+		self.mem[self.BLUE_OFFSET + whichbyte] += bitRead(color.r,0)*posInByteHelp[posInByte]
 
 posInByteHelp = [1,2,4,8,16,32,64,128]
 
 def bitWrite(i, bitnr, value):
-    return i | (1 << bitnr) if value else i & (1<<bitnr)
+	return i | (1 << bitnr) if value else i & (1<<bitnr)
 
 def bitRead(i, bitnr):
-    return (i >> (bitnr))&1
+	return (i >> (bitnr))&1
 
-def mainloop():
-	t0 = time.time()
-	fr = 0
-	tf0 = 0
-	spi = spidev.SpiDev()
-	spi.open(0, 0)
-	spi.max_speed_hz = 8000000
-	while not quit:
-		cbuf = buf
-		for i in range(0, 64, 8):
-			spi.xfer(list(cbuf.byte_red0[i:i+8]) + list(cbuf.byte_green0[i:i+8]) + list(cbuf.byte_blue0[i:i+8]) + list([1 << (i >> 3)]))
-			#time.sleep(0.00001)
-		fr += 1
-		if fr == 400:
-			t = time.time() - t0
-			fps = fr / (t - tf0)
-			print('Driver FPS', fps)
-			fr = 0
-			tf0 = t
+class Driver:
+	def __init__(self):
+		self.buf = RawArray('B', Buffer.MEM_SIZE)
+		self.sp = None
+		self.quit = RawValue('B', 0)
 
-	spi.xfer2([0] * 25)
+	@staticmethod
+	def _mainloop(buf, quit):
+		t0 = time.time()
+		fr = 0
+		tf0 = 0
+		spi = spidev.SpiDev()
+		spi.open(0, 0)
+		spi.max_speed_hz = 8000000
+		while not quit:
+			for i in range(0, 64, 8):
+				red_i = Buffer.RED_OFFSET + i
+				green_i = Buffer.GREEN_OFFSET + i
+				blue_i = Buffer.BLUE_OFFSET + i
+				spi.xfer(list(buf[red_i:red_i+8]) + list(buf[green_i:green_i+8]) + list(buf[blue_i:blue_i+8]) + list([1 << (i >> 3)]))
+				#time.sleep(0.00001)
+			fr += 1
+			if fr == 400:
+				t = time.time() - t0
+				fps = fr / (t - tf0)
+				print('Driver FPS', fps)
+				fr = 0
+				tf0 = t
+		spi.xfer2([0] * 25)
+
+	@staticmethod
+	def make_frame():
+		return Buffer()
+
+	def fill(self, frame):
+		self.buf[:] = frame.mem
+
+	def run(self):
+		if self.sp is None:
+			self.quit = 0
+			self.sp = multiprocessing.Process(target=self._mainloop, args=(self.buf, self.quit))
+			self.sp.daemon = True
+			self.sp.start()
+
+	def stop(self):
+		if self.sp is not None:
+			self.quit = 1
+			self.sp.join()
 
